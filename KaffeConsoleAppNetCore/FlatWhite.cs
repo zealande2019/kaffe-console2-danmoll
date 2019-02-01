bc6ce0c243e6acbb33ce3c776a739196eb5c4379
DanMoll <dani948d@edu.easj.dk>
2019-02-01 13:30:04 +0100

    Added FlatWhite class.

---
+++ KaffeConsoleAppNetCore/FlatWhite.cs
@@ -0,0 +1,25 @@
+﻿using kaffe;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KaffeConsoleAppNetCore
+{
+    public class FlatWhite : Kaffe, Imælk
+    {
+        public override int Pris()
+        {
+            return 45;
+        }
+
+        public int MlMælk()
+        {
+            return 160;
+        }
+
+        public override string Styrke()
+        {
+            return "Mild";
+        }
+    }
+}
